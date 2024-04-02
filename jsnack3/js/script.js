c8189e8fa48c844e0e934b6827594d7c4382757f
WalterCorsini <corsini.walter@gmail.com>
2024-04-02 16:52:16 +0200

    add: jsnack3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,11 +1,16 @@
+// INPUT
+// declaration variable
 const listNum = [];
 let sum = 0;
 let flag = false;
+
+// input number from user
 for(let i = 0; i < 10; i++){
     listNum.push(parseInt(prompt("inserisci un numero")));
 }
-let contatore = 0;
-// console.log(listNum);
+
+// LOGIC
+// control isNaN
 for(let i = 0; i < 10; i++){
     if(isNaN(listNum[i])){
         flag = true;
@@ -13,6 +18,7 @@ for(let i = 0; i < 10; i++){
     }
 }
 
+// if isNan Error else Sum
 if(flag === true){
     alert("dati inseriti non corretti!!");
 } else{
@@ -21,6 +27,6 @@ if(flag === true){
     }
 }
 
-
+// OUTPUT
 alert("ecco a te la somma: "+ sum);
 
