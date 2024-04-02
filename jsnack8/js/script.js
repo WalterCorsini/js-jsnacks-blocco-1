383b3804e4ebb9bd034787cfc7a6665149b9c54c
WalterCorsini <corsini.walter@gmail.com>
2024-04-02 19:41:17 +0200

    add: jsnack8

--- jsnack8/js/script.js
+++ jsnack8/js/script.js
@@ -6,35 +6,35 @@ let flagIsNan = false;
 let flagNumberCorrect = false;
 let sum = 0;
 let cont = 0;
-console.log(userNumber);
+
 // LOGIC
 //  lazy evaluation condition
 if (userNumber == null || userNumber == "") {
   alert("campo vuoto");
 } else {
-  if (isNaN(userNumber) && flagIsNan == false) {
-    flagIsNan = true;
-  }
-  if (userNumber.length < 4 && flagIsNan == false) {
-    flagNumberCorrect = true;
-  }
-  //  lazy evaluation condition
+    if (isNaN(userNumber) && flagIsNan == false) {
+        flagIsNan = true;
+    }
+    if (userNumber.length < 4 && flagIsNan == false) {
+        flagNumberCorrect = true;
+    }
+//  lazy evaluation condition
 
-  // sum
-  for (
-    let i = 0;
-    i < userNumber.length && flagIsNan == false && flagNumberCorrect == false;
-    i++
-  ) {
-    sum += parseInt(userNumber[i]);
-  }
+// sum
+    for (
+        let i = 0;
+        i < userNumber.length && flagIsNan == false && flagNumberCorrect == false;
+        i++
+    ) {
+        sum += parseInt(userNumber[i]);
+    }
 
-  // OUTPUT
-  if (flagIsNan == false && flagNumberCorrect == false) {
-    alert(`la somma del tuo numero è di ${sum}`);
-  } else if (flagIsNan) {
-    alert("non hai inserito i dati correttamente");
-  } else if (flagNumberCorrect) {
-    alert("il tuo numero non è da 4 cifre");
-  }
+// OUTPUT
+    if (flagIsNan == false && flagNumberCorrect == false) {
+        alert(`la somma del tuo numero è di ${sum}`);
+    } else if (flagIsNan) {
+        alert("non hai inserito i dati correttamente");
+    } else if (flagNumberCorrect) {
+        alert("il tuo numero non è da 4 cifre");
+    }
 }
