339f339b69bd719de7b6b8e27ea6f3e635c03718
WalterCorsini <corsini.walter@gmail.com>
2024-04-02 19:54:21 +0200

    fix: add evaluation and add start number

--- jsnack9/js/script.js
+++ jsnack9/js/script.js
@@ -1,8 +1,18 @@
 // INPUT
 // declaration variable
+userNumber = parseInt(prompt("da che numero vuoi che inizi a fare la somma per i successivi 10?"));
 let sum = 0;
+let flag = false;
 
-for(let i = 0; i <= 10; i++){
-    sum += i;
+// LOGIC
+// lazy evaluation condition
+if(userNumber == null || isNaN(userNumber) || userNumber == ""){
+    alert("dati inseriti non corretti");
+} else{
+    for(let i = userNumber; i < userNumber+10; i++){
+        sum += i;
+        console.log(sum);
+    }
+// OUTPUT
+    alert(`la somma totale dal numero da cui siamo partiti "${userNumber}" ai successivi 10 numeri è di: ${sum}`);
 }
-alert(sum);
